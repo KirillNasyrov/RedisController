b6345848c2b17fc47da7a76cf4c990196aabd973
Kirill Nasyrov <90870970+KirillNasyrov@users.noreply.github.com>
2023-04-30 18:10:54 +0300

    development

--- RedisController/StartPage.xaml.cs
+++ RedisController/StartPage.xaml.cs
@@ -7,8 +7,10 @@ public partial class StartPage : ContentPage
 	List<RedisDataBaseConfiguration> configs;
 
     public StartPage()
-	{
-		configs = new List<RedisDataBaseConfiguration>();
+    {
+        InitializeComponent();
+
+        configs = new List<RedisDataBaseConfiguration>();
 		configs.Add(new RedisDataBaseConfiguration("redis1", "localhost", "32778"));
         configs.Add(new RedisDataBaseConfiguration("redis2", "localhost", "32779"));
         configs.Add(new RedisDataBaseConfiguration("redis3", "localhost", "32780"));
@@ -16,7 +18,6 @@ public partial class StartPage : ContentPage
         configs.Add(new RedisDataBaseConfiguration("redis5", "localhost", "32782"));
         configs.Add(new RedisDataBaseConfiguration("redis6", "localhost", "32783"));
         configs.Add(new RedisDataBaseConfiguration("redis7", "localhost", "32784"));
-        InitializeComponent();
         tableOfConfigs.ItemsSource = configs;
     }
 
